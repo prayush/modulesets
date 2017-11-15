00d2a11e7147480ad2d8bb8dc23f0202dd36edb6
Prayush Kumar <prayush.kumar@ligo.org>
2017-11-15 11:03:23 -0800

    enabling installation of lal to user-specified directory instead of ~/pe

--- install_lalinference_cbc.sh
+++ install_lalinference_cbc.sh
@@ -1,79 +1,81 @@
 #!/bin/bash
 
+export INSTALL_DIR=${1}
+
 # Create pe directory where the installation will be
-mkdir -p ~/pe
-cd ~/pe
+mkdir -p ${INSTALL_DIR}
+cd ${INSTALL_DIR}
 
 # Download module sets for jhbuild
-if [ ! -d ~/pe/modulesets ]; then
-  git clone git://github.com/vivienr/modulesets.git ~/pe/modulesets
+if [ ! -d ${INSTALL_DIR}/modulesets ]; then
+  git clone git://github.com/vivienr/modulesets.git ${INSTALL_DIR}/modulesets
 fi
-cd ~/pe/modulesets
+cd ${INSTALL_DIR}/modulesets
 git pull
 
 # Download and install jhbuild
-mkdir -p ~/pe/src
-if [ ! -d ~/pe/src/jhbuild ]; then
-  git clone git://git.gnome.org/jhbuild ~/pe/src/jhbuild
-  cd ~/pe/src/jhbuild
-  ./autogen.sh --prefix=~/pe/.local/
+mkdir -p ${INSTALL_DIR}/src
+if [ ! -d ${INSTALL_DIR}/src/jhbuild ]; then
+  git clone git://git.gnome.org/jhbuild ${INSTALL_DIR}/src/jhbuild
+  cd ${INSTALL_DIR}/src/jhbuild
+  ./autogen.sh --prefix=${INSTALL_DIR}/.local/
   make
   make install
 fi
 
 # Set up jhbuild configuration files
-mkdir -p ~/pe/.config && cd ~/pe/.config
-if [ ! -e ~/pe/.config/jhbuildrc ]; then
-  ln -s ~/pe/modulesets/jhbuildrc
+mkdir -p ${INSTALL_DIR}/.config && cd ${INSTALL_DIR}/.config
+if [ ! -e ${INSTALL_DIR}/.config/jhbuildrc ]; then
+  ln -s ${INSTALL_DIR}/modulesets/jhbuildrc
 fi
 
-echo "prefix = '$HOME/pe/local/'" > ~/pe/.config/pe.jhbuildrc
-echo "modulesets_dir = '$HOME/pe/modulesets'" >> ~/pe/.config/pe.jhbuildrc
-echo "checkoutroot = '$HOME/pe/src'" >> ~/pe/.config/pe.jhbuildrc
-echo "buildroot = '$HOME/pe/local/build'" >> ~/pe/.config/pe.jhbuildrc
-echo "branches['lal'] = (None,'lalinference_o2')" >> ~/pe/.config/pe.jhbuildrc
-echo "branches['laldetchar'] = (None,'lalinference_o2')" >> ~/pe/.config/pe.jhbuildrc
-echo "branches['lalframe'] = (None,'lalinference_o2')" >> ~/pe/.config/pe.jhbuildrc
-echo "branches['lalmetaio'] = (None,'lalinference_o2')" >> ~/pe/.config/pe.jhbuildrc
-echo "branches['lalxml'] = (None,'lalinference_o2')" >> ~/pe/.config/pe.jhbuildrc
-echo "branches['lalburst'] = (None,'lalinference_o2')" >> ~/pe/.config/pe.jhbuildrc
-echo "branches['lalpulsar'] = (None,'lalinference_o2')" >> ~/pe/.config/pe.jhbuildrc
-echo "branches['lalstochastic'] = (None,'lalinference_o2')" >> ~/pe/.config/pe.jhbuildrc
-echo "branches['lalinspiral'] = (None,'lalinference_o2')" >> ~/pe/.config/pe.jhbuildrc
-echo "branches['lalsimulation'] = (None,'lalinference_o2')" >> ~/pe/.config/pe.jhbuildrc
-echo "branches['lalinference'] = (None,'lalinference_o2')" >> ~/pe/.config/pe.jhbuildrc
-echo "branches['lalapps'] = (None,'lalinference_o2')" >> ~/pe/.config/pe.jhbuildrc
-echo "branches['glue'] = (None,'lalinference_o2')" >> ~/pe/.config/pe.jhbuildrc
-echo "branches['pylal'] = (None,'lalinference_o2')" >> ~/pe/.config/pe.jhbuildrc
-echo "branches['ligo'] = (None,'lalinference_o2')" >> ~/pe/.config/pe.jhbuildrc
-echo "" >> ~/pe/.config/pe.jhbuildrc
-echo "intel_executables = ['icc','icpc','ifort','mpiicc','mpiicpc','mpiifort','xiar']" >> ~/pe/.config/pe.jhbuildrc
-echo "" >> ~/pe/.config/pe.jhbuildrc
-echo "from distutils.spawn import find_executable" >> ~/pe/.config/pe.jhbuildrc
-echo "def is_in_path(name):" >> ~/pe/.config/pe.jhbuildrc
-echo '    """Check whether `name` is on PATH."""' >> ~/pe/.config/pe.jhbuildrc
-echo "    return find_executable(name) is not None" >> ~/pe/.config/pe.jhbuildrc
-echo "" >> ~/pe/.config/pe.jhbuildrc
-echo "if all([is_in_path(name) for name in intel_executables]):" >> ~/pe/.config/pe.jhbuildrc
-echo "   icc = True" >> ~/pe/.config/pe.jhbuildrc
-echo "" >> ~/pe/.config/pe.jhbuildrc
-echo "del name" >> ~/pe/.config/pe.jhbuildrc
-echo "del intel_executables" >> ~/pe/.config/pe.jhbuildrc
-echo "" >> ~/pe/.config/pe.jhbuildrc
-echo "os.environ['LAL_DATA_PATH'] = '$HOME/ROM_data/'" >> ~/pe/.config/pe.jhbuildrc
-echo "" >> ~/pe/.config/pe.jhbuildrc
+echo "prefix = '${INSTALL_DIR}/local/'" > ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "modulesets_dir = '${INSTALL_DIR}/modulesets'" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "checkoutroot = '${INSTALL_DIR}/src'" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "buildroot = '${INSTALL_DIR}/local/build'" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "branches['lal'] = (None,'lalinference_o2')" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "branches['laldetchar'] = (None,'lalinference_o2')" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "branches['lalframe'] = (None,'lalinference_o2')" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "branches['lalmetaio'] = (None,'lalinference_o2')" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "branches['lalxml'] = (None,'lalinference_o2')" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "branches['lalburst'] = (None,'lalinference_o2')" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "branches['lalpulsar'] = (None,'lalinference_o2')" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "branches['lalstochastic'] = (None,'lalinference_o2')" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "branches['lalinspiral'] = (None,'lalinference_o2')" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "branches['lalsimulation'] = (None,'lalinference_o2')" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "branches['lalinference'] = (None,'lalinference_o2')" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "branches['lalapps'] = (None,'lalinference_o2')" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "branches['glue'] = (None,'lalinference_o2')" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "branches['pylal'] = (None,'lalinference_o2')" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "branches['ligo'] = (None,'lalinference_o2')" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "intel_executables = ['icc','icpc','ifort','mpiicc','mpiicpc','mpiifort','xiar']" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "from distutils.spawn import find_executable" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "def is_in_path(name):" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo '    """Check whether `name` is on PATH."""' >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "    return find_executable(name) is not None" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "if all([is_in_path(name) for name in intel_executables]):" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "   icc = True" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "del name" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "del intel_executables" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "os.environ['LAL_DATA_PATH'] = '$HOME/ROM_data/'" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
+echo "" >> ${INSTALL_DIR}/.config/pe.jhbuildrc
 
 # Install lalsuite from anonymous repository
-~/pe/.local/bin/jhbuild -f ~/pe/.config/jhbuildrc --no-interact tinderbox --output=$HOME/public_html/pe/build/ lalsuite
+${INSTALL_DIR}/.local/bin/jhbuild -f ${INSTALL_DIR}/.config/jhbuildrc --no-interact tinderbox --output=$HOME/public_html/pe/build/ lalsuite
 
 # Move bayeswave as it gets build in source directory
-cp ~/pe/src/bayeswave/src/bayeswave ~/pe/local/bin/ 
+cp ${INSTALL_DIR}/src/bayeswave/src/bayeswave ${INSTALL_DIR}/local/bin/ 
 
 ## If needed, install additional packages:
-# ~/pe/.local/bin/jhbuild -f ~/pe/.config/jhbuildrc run $SHELL --noprofile --norc
-# pip install <package> --install-option="--prefix=~/pe/local/"
+# ${INSTALL_DIR}/.local/bin/jhbuild -f ${INSTALL_DIR}/.config/jhbuildrc run $SHELL --noprofile --norc
+# pip install <package> --install-option="--prefix=${INSTALL_DIR}/local/"
 
 # Create initisalisation script
-echo '#!/bin/bash' > ~/pe/lalinference_o2.sh
-echo "$HOME/pe/.local/bin/jhbuild -f $HOME/pe/.config/jhbuildrc run \$SHELL --noprofile --norc" >> ~/pe/lalinference_o2.sh
-chmod a+x ~/pe/lalinference_o2.sh
+echo '#!/bin/bash' > ${INSTALL_DIR}/lalinference_o2.sh
+echo "${INSTALL_DIR}/.local/bin/jhbuild -f ${INSTALL_DIR}/.config/jhbuildrc run \$SHELL --noprofile --norc" >> ${INSTALL_DIR}/lalinference_o2.sh
+chmod a+x ${INSTALL_DIR}/lalinference_o2.sh
